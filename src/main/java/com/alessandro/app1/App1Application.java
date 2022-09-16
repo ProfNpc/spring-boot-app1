97426edea816b3590e8800c413bdf2bc62a4acc4
Alessandro <professornpc@gmail.com>
2022-09-15 21:05:18 -0300

    Add método olaMundo()

--- src/main/java/com/alessandro/app1/App1Application.java
+++ src/main/java/com/alessandro/app1/App1Application.java
@@ -2,7 +2,10 @@ package com.alessandro.app1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class App1Application {
 
@@ -10,4 +13,8 @@ public class App1Application {
 		SpringApplication.run(App1Application.class, args);
 	}
 
+	@GetMapping("/")
+	public String olaMundo() {
+		return "Olá Mundo!!!";
+	}
 }
